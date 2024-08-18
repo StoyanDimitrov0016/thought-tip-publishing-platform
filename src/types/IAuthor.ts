ded88affdb7029b4eb9f76cecab4617c9dfb01e2
StoyanDimitrov0016 <stoyan1020@gmail.com>
2024-08-18 21:30:44 +0300

    interfaces: Refactor Author interfaces

--- src/types/IAuthor.ts
+++ src/types/IAuthor.ts
@@ -1,13 +1,17 @@
-export interface IAuthorDetailsPreview {
+export interface IAuthorPreview {
+  id: string;
   firstName: string;
   lastName: string;
   profilePicture: string;
   rank: string;
+  username: string;
 }
 
-export interface IAuthorDetailsExtended extends IAuthorDetailsPreview {
-  badges: string[];
+export interface IAuthorDetail extends IAuthorPreview {
+  avatarPicture: string;
   bio: string;
+  accountCreationDate: Date;
+  articles: string[];
   followersCount: number;
   followingCount: number;
 }

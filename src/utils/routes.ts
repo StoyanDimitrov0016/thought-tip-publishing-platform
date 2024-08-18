d77bae9056ff0b0c78b8c5f1c79f60ffdde2a808
StoyanDimitrov0016 <stoyan1020@gmail.com>
2024-08-18 21:35:17 +0300

    feat(routes): Add new routes to the centralized ROUTES object

--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -3,4 +3,11 @@ export const ROUTES = {
   ARTICLE: "/articles/:articleId",
   LOGIN: "/auth/login",
   REGISTER: "/auth/register",
+  USER_NOTIFICATIONS: "/my-notifications",
+  CREATE_ARTICLE: "/create-article",
+  USER_PROFILE: "/my-profile",
+  USER_PROFILE_DETAILS: "/my-profile/details",
+  USER_PROFILE_PREFERENCES: "/my-profile/preferences",
+  USER_PROFILE_USER_ARTICLES: "/my-profile/my-articles",
+  USER_PROFILE_SAVED_ARTICLES: "/my-profile/saved-articles",
 };

b8570e7f85bf027edd1928b6bab635a9bd291285
StoyanDimitrov0016 <stoyan1020@gmail.com>
2024-09-25 18:18:07 +0300

    feat: implement Clerk in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { ClerkProvider } from "@clerk/clerk-react";
 
-createRoot(document.getElementById('root')!).render(
+import "./styles/reset.css";
+import "./styles/colors.css";
+import "./styles/utilities.css";
+
+import router from "./router.tsx";
+import { ROUTES } from "./utils/routes.ts";
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Clerk publishable key missing!");
+}
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl={ROUTES.HOME}>
+      <RouterProvider router={router} />
+    </ClerkProvider>
+  </StrictMode>
+);

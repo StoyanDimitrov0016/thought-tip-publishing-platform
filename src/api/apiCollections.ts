1ca6ab6ed37c091b2fefffe751e115917e2782b8
StoyanDimitrov0016 <stoyan1020@gmail.com>
2024-10-06 22:27:30 +0300

    feat: Create a centralized API collection names in a single export for easier reference

---
+++ src/api/apiCollections.ts
@@ -0,0 +1,8 @@
+export const API_COLLECTIONS = {
+  articlesCore: "articles",
+  articlesComments: "article-comments",
+  articlesReplyComments: "reply-comments",
+  articlesLikes: "likes",
+  articlesSaved: "saved-articles",
+  resources: "resources",
+};

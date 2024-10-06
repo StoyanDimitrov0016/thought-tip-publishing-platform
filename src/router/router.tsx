a0849958364924225f81097a474a2b2984d35b84
StoyanDimitrov0016 <stoyan1020@gmail.com>
2024-10-06 22:29:39 +0300

    chore: relocate front-end router and routes to centralized 'router' folder for improved organization

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,16 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import { SignedIn, SignedOut, RedirectToSignIn } from "@clerk/clerk-react";
 
-import { ROUTES } from "./utils/routes";
+import { ROUTES } from "./routes";
 
-import MainLayout from "./layouts/main-layout/MainLayout";
-import HomePage from "./pages/home-page/HomePage";
-import ArticlePage from "./pages/article-page/ArticlePage";
-import AuthorPage from "./pages/author-page/AuthorPage";
-import CreateArticlePage from "./pages/create-article-page/CreateArticlePage";
-import SignInPage from "./pages/sign-in-page/SignInPage";
-import SignUpPage from "./pages/sign-up-page/SignUpPage";
-import UserProfilePage from "./pages/user-profile-page/UserProfilePage";
+import MainLayout from "../layouts/main-layout/MainLayout";
+import HomePage from "../pages/home-page/HomePage";
+import ArticlePage from "../pages/article-page/ArticlePage";
+import AuthorPage from "../pages/author-page/AuthorPage";
+import CreateArticlePage from "../pages/create-article-page/CreateArticlePage";
+import SignInPage from "../pages/sign-in-page/SignInPage";
+import SignUpPage from "../pages/sign-up-page/SignUpPage";
+import UserProfilePage from "../pages/user-profile-page/UserProfilePage";
 
 const router = createBrowserRouter([
   {

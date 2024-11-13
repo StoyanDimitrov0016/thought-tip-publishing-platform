90a190c437f8d97fd4a2a1d9ce96adb24077e160
StoyanDimitrov0016 <stoyan1020@gmail.com>
2024-11-13 20:43:22 +0200

    feat: add UserRole enum to enhance role management in auth API response

--- src/types/auth/IAuthUser.ts
+++ src/types/auth/IAuthUser.ts
@@ -1,9 +1,18 @@
+// Partial user data returned in the auth API response.
+// Contains only essential fields for local storage, excluding less critical info like bio.
+
+enum UserRole {
+  Regular = "regular",
+  Moderator = "moderator",
+  Admin = "admin",
+}
+
 export interface IAuthUser {
   id: string;
   externalUserId: string | null;
   email: string;
   username: string;
-  role: "regular" | "moderator" | "admin";
+  role: UserRole;
   profilePicture: string | null;
   verificationLevel: number;
   articleLimit: number;

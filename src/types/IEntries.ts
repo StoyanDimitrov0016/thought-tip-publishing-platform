25443759797fd6693118756bbbd840d1318e7164
StoyanDimitrov0016 <stoyan1020@gmail.com>
2024-11-24 23:36:11 +0200

    feat: add user, profile, and article interfaces with complementary enums for modularity

--- src/types/IEntries.ts
+++ src/types/IEntries.ts
@@ -12,7 +12,18 @@ enum SegmentationStatus {
   ARCHIVED = "archived",
 }
 
-// -- Segmentation Partial Interface
+enum VerificationLevel {
+  INITIAL = 0,
+  PROFILE_COMPLETE = 1,
+  SUPPORTER = 2,
+}
+
+enum UserRoles {
+  REGULAR = "regular",
+  MODERATOR = "moderator",
+  ADMIN = "admin",
+}
+
 interface ISegmentationPartial {
   id: string; // Reference ID
   slug: string; // Slug for the referenced entry
@@ -38,4 +49,71 @@ export interface ITopicEntry extends ISegmentationEntryBase {
 
 export interface ITagEntry extends ISegmentationEntryBase {
   topicId: string;
+}
+
+export interface IUserEntry extends IBaseEntry {
+  externalUserId: string | null;
+  email: string;
+  username: string;
+  role: UserRoles;
+  verificationLevel: VerificationLevel;
+}
+
+export interface IProfileEntry extends IBaseEntry {
+  id: string;
+  userId: string; // Link to the corresponding user from IUserEntry
+  firstName: string | null;
+  lastName: string | null;
+  profilePicture: string | null;
+  bio: string | null;
+  followers: number;
+  followings: number;
+  articleLimit: number;
+  publishedArticles: number;
+  walletId: string | null;
+}
+
+export interface IArticleEntry {
+  title: string;
+  slug: string;
+  thumbnail: string;
+  hook: string;
+  content: string;
+  category: string;
+  topics: string[];
+  tags: string[];
+  readingTime: number;
+  author: string;
+  paywalled: boolean;
+  discussion: boolean;
+}
+
+export interface IArticleFullEntry {
+  title: string;
+  slug: string;
+  thumbnail: string;
+  hook: string;
+  content: string;
+  category: string;
+  topics: string[];
+  tags: string[];
+  readingTime: number;
+  author: string;
+  paywalled: boolean;
+  discussion: boolean;
+}
+export interface IArticlePartialEntry {
+  id: string;
+  title: string;
+  slug: string;
+  thumbnail: string;
+  hook: string;
+  content: string;
+  author: string; // ---
+  category: string;
+  topics: string[];
+  tags: string[];
+  readingTime: number;
+  charge: number;
+  discussion: boolean;
 }

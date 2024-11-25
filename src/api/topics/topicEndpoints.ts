fc684f40f144da19894b0ceebbdeca5ebb3179c5
StoyanDimitrov0016 <stoyan1020@gmail.com>
2024-11-25 20:44:13 +0200

    remove legacy files

--- src/api/topics/topicEndpoints.ts
+++
@@ -1,6 +0,0 @@
-import { API_COLLECTIONS } from "../apiCollections";
-
-export const TOPICS_AND_TAGS_ENDPOINTS = {
-  getTopics: () => `/${API_COLLECTIONS.resources}/topics`,
-  getTagsByTopic: (topic: string) => `/${API_COLLECTIONS.resources}/tags?topic=${topic}`,
-};

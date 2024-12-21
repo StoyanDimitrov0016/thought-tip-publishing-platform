15a30cbaa8b395f259e863a63e4e40a9e3c34503
StoyanDimitrov0016 <stoyan1020@gmail.com>
2024-12-21 23:46:20 +0200

    chore: change the base url's port, add credentials to options and remove for now the authorization

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,23 +1,25 @@
 import axios from "axios";
 import tokenManager from "./tokenManager";
 
-const BASE_URL = "http://localhost:8888/api/v1";
+const BASE_URL = "http://localhost:5000/api/v1";
 
 const apiClient = axios.create({
   baseURL: BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
+  withCredentials: true,
 });
 
-apiClient.interceptors.request.use(async (config) => {
-  const token = await tokenManager();
+// TODO: Fix the hook invoking because it is not in a function
+// apiClient.interceptors.request.use(async (config) => {
+//   const token = await tokenManager();
 
-  if (token) {
-    config.headers.Authorization = token;
-  }
+//   if (token) {
+//     config.headers.Authorization = token;
+//   }
 
-  return config;
-});
+//   return config;
+// });
 
 export default apiClient;

740bc3eea6297f19d2b664485a2c6a588a3e00f0
StoyanDimitrov0016 <stoyan1020@gmail.com>
2025-02-03 21:46:32 +0200

    fix(paths): standardize routing structure and improve profile route nesting

--- src/config/paths.ts
+++ src/config/paths.ts
@@ -3,22 +3,30 @@ export const PATHS = {
   HOME: "",
 
   // Author paths:
-  AUTHOR: (authorId: string) => `/authors/${authorId}`,
+  AUTHOR: `/authors/:authorId`,
 
   // Article paths:
-  ARTICLE: (articleId: string) => `/articles/${articleId}`,
+  ARTICLE: "/articles/:articleId",
   CREATE_ARTICLE: "/create-article",
-  UPDATE_ARTICLE: (articleId: string) => `/update-article/${articleId}`,
+  UPDATE_ARTICLE: "/update-article/:articleId",
 
   // Auth paths:
-  LOGIN: "/auth/login",
-  REGISTER: "/auth/register",
-  LOGOUT: "/auth/logout",
+  LOGIN: "/login",
+  REGISTER: "/register",
+  LOGOUT: "/logout",
 
   // Profile paths:
   MY_PROFILE: "/my-profile",
   MY_ARTICLES: "/my-profile/my-articles",
   MY_BOOKMARKS: "/my-profile/my-bookmarks",
-  ACCOUNT_SETTINGS: "/my-profile/account-settings", // Renamed from MY_SETTINGS
+  MY_DETAILS: "/my-profile/details",
+
+  // Profile nested paths considering react router dom requirements
+  MY_PROFILE_MY_ARTICLES: "my-articles",
+  MY_PROFILE_MY_BOOKMARKS: "my-bookmarks",
+  MY_PROFILE_MY_DETAILS: "details",
+
+  // For future implementa:
+  ACCOUNT_SETTINGS: "/my-profile/account-settings",
   NOTIFICATIONS: "/my-profile/notifications",
 };

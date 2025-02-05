a7fff3058b994f208e79130ae4a5a6fcc6581980
StoyanDimitrov0016 <stoyan1020@gmail.com>
2025-02-05 10:49:31 +0200

    refactor: implement redirect specific paths

--- src/guards/AuthGuard.tsx
+++ src/guards/AuthGuard.tsx
@@ -1,12 +1,12 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuthContext } from "../contexts/authContext";
-import { ROUTES } from "../router/routes";
+import { REDIRECT_PATHS } from "../config/paths";
 
 export default function AuthGuard() {
   const { isAuth, isLoading } = useAuthContext();
 
   if (isLoading) return null;
-  if (!isAuth) return <Navigate to={ROUTES.LOGIN} replace />;
+  if (!isAuth) return <Navigate to={REDIRECT_PATHS.AUTH_GUARD_REDIRECT} replace />;
 
   return <Outlet />;
 }

5a0198cf065dcc1029f3d36beaeaf5c1f67f9456
StoyanDimitrov0016 <stoyan1020@gmail.com>
2025-02-07 23:24:49 +0200

    chore: add function for current full year retrieval and direct return in other functions instead of assigning the result into a variable that will be returned for simplicity

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,22 +1,24 @@
 const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
+export function getCurrentYear() {
+  return new Date().getFullYear();
+}
+
 export const getPublishedDate = (createdAt: string) => {
   const date = new Date(createdAt);
 
   const day = date.getDate();
   const month = months[date.getMonth()];
-  const year = date.getFullYear();
+  const year = getCurrentYear();
 
-  const publishedDate = `${day} ${month} ${year}`;
-  return publishedDate;
+  return `${day} ${month} ${year}`;
 };
 
 export const checkIsUpdated = (createdAt: string, updateAt: string) => {
   const created = new Date(createdAt);
   const updated = new Date(updateAt);
 
-  const isUpdated = created.getTime() - updated.getTime() !== 0;
-  return isUpdated;
+  return created.getTime() - updated.getTime() !== 0;
 };
 
 export const parseReadingTimeDuration = (readingTime: number) => {

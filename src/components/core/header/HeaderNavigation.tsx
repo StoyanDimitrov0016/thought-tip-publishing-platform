17f99a8168d8aad8d18a0c86d9f279e3311e8322
StoyanDimitrov0016 <stoyan1020@gmail.com>
2025-02-17 22:58:14 +0200

    chore: extract the newly provided values from auth context and change "create" to "write" CTA button

--- src/components/core/header/HeaderNavigation.tsx
+++ src/components/core/header/HeaderNavigation.tsx
@@ -1,30 +1,30 @@
 import { Link } from "react-router-dom";
-import { useAuthContext } from "../../../contexts/authContext";
+import { useAuthContext } from "../../../contexts/auth-context";
 import { PATHS } from "../../../config/paths";
 import { RxAvatar } from "react-icons/rx";
 import { FaPencilAlt } from "react-icons/fa";
 
 export default function HeaderNavigation() {
-  const { user, isLoading, isAuth } = useAuthContext();
+  const { user, isFetchingUser } = useAuthContext();
 
-  if (isLoading) return null; // Avoid rendering while loading
+  if (isFetchingUser) return null;
 
   return (
     <nav className="header-nav">
       <ul className="header-nav-list">
-        {isAuth ? (
+        {user ? (
           <>
             <li className="header-nav-item">
               <Link to={PATHS.CREATE_ARTICLE} className="header-nav-link">
                 <FaPencilAlt />
-                <span>Create</span>
+                <span>Write</span>
               </Link>
             </li>
             <li className="header-nav-item">
               <Link to={PATHS.MY_PROFILE} className="header-nav-link header-profile">
                 <div className="header-avatar">
-                  {user?.avatar ? (
-                    <img src={user?.avatar} alt="User Avatar" className="avatar" />
+                  {user.avatar ? (
+                    <img src={user.avatar} alt="User Avatar" className="avatar" />
                   ) : (
                     <RxAvatar className="avatar-icon" />
                   )}

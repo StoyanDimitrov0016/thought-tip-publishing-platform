4549999dddcb8916a2bf055130bb32b9724b2b51
StoyanDimitrov0016 <stoyan1020@gmail.com>
2025-02-26 23:56:04 +0200

    chore: remove unnecessary user profile routes

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,6 +1,6 @@
 export const ROUTES = {
   HOME: "/",
-  ARTICLE: "/articles/:username/:articleSlug",
+  ARTICLE: "/articles/:articleId",
   LOGIN: "/auth/login",
   REGISTER: "/auth/register",
   LOGOUT: "/auth/register",
@@ -8,8 +8,4 @@ export const ROUTES = {
   USER_NOTIFICATIONS: "/my-notifications",
   CREATE_ARTICLE: "/create-article",
   USER_PROFILE: "/my-profile",
-  USER_PROFILE_DETAILS: "/my-profile/details",
-  USER_PROFILE_PREFERENCES: "/my-profile/preferences",
-  USER_PROFILE_USER_ARTICLES: "/my-profile/my-articles",
-  USER_PROFILE_SAVED_ARTICLES: "/my-profile/saved-articles",
 };
